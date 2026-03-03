0eee16e79af0f5220892221beaf0d13131dcb891
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-03 13:45:25 +0100

    add credits

--- userscript.js
+++ userscript.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name        Hide Users Messages cloud.microsoft
+// @namespace   Violentmonkey Scripts
+// @match       https://teams.cloud.microsoft/*
+// @grant       none
+// @version     1.0
+// @author      thomaslinux9 && perplexity.ai
+// @description 03/03/2026 11:38:20
+// @run-at      document-idle
+// ==/UserScript==
+
+// add a user familyname in the hideUsers list to hide the user
 const hideUsers = ["USER1", "USER2"];
 
 window.addEventListener("load", () => {
