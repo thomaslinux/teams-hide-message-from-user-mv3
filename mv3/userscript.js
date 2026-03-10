fa822b49992121c6cde48e170688c2419d0d13b9
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-03-10 14:01:34 +0100

    instructions and update code

--- mv3/userscript.js
+++ mv3/userscript.js
@@ -3,52 +3,49 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://teams.cloud.microsoft/*
 // @grant       none
-// @version     2026.03.03.14.05
+// @version     2026.03.10.14.01
 // @author      thomaslinux9 && perplexity.ai
 // @description 03/03/2026 14:05:00
 // @run-at      document-idle
 // ==/UserScript==
 
-const hideUsers = ["USER1", "USER2"];
+// const hideUsers = ["USER1", "USER2"];
 
-window.addEventListener("load", () => {
-  console.log("Hide users:", hideUsers);
+// window.addEventListener("load", () => {
+//   console.log("Hide users:", hideUsers);
 
-  // Build the CSS selector from hideUsers
-  const selector = hideUsers
-    .map(
-      (u) =>
-        `div[class*="fui-ChatMessage"]:has(img[src*="${u}"]) div[data-message-content]`,
-    )
-    .join(",\n");
+//   // Build the CSS selector from hideUsers
+//   const selector = hideUsers
+//     .map(
+//       (u) =>
+//         `div[class*="fui-ChatMessage"]:has(img[src*="${u}"]) div[data-message-content]`,
+//     )
+//     .join(",\n");
 
-  const style = document.createElement("style");
-  style.textContent = `
-    ${selector} {
-      display: none !important;
-    }
-  `;
-  document.head.appendChild(style);
-});
+//   const style = document.createElement("style");
+//   style.textContent = `
+//     ${selector} {
+//       display: none !important;
+//     }
+//   `;
+//   document.head.appendChild(style);
+// });
 
-// javascript select users using name
-let users = [];
-document
-  .querySelectorAll("span[data-tid*=message-author-name]")
-  .forEach((span) => {
-    if ((span.textContent.includes = "LASTNAME")) {
-      users.push(span);
-    }
-  });
+// // javascript select users using name
+// let users = [];
+// document
+//   .querySelectorAll("span[data-tid*=message-author-name]")
+//   .forEach((span) => {
+//     if ((span.textContent.includes = "LASTNAME")) {
+//       users.push(span);
+//     }
+//   });
 
 // document.querySelector('div[data-tid="message-pane-list-runway"]').addEventListener('e', () => {
 //   console.log('event change on the element');
 // })
 
-const usersToHide = [];
-usersToHide.push("user1");
-usersToHide.push("user2");
-console.table(usersToHide);
+let usersToHide = [];
 
 // listen changes on the DOM
 const target = document.querySelector(
@@ -62,17 +59,17 @@ if (target) {
       document
         .querySelectorAll("span[data-tid*=message-author-name]")
         .forEach((span) => {
-          console.log("span.innerText", span.innerText);
-          console.log("span.classList", span.classList);
+          // console.log("span.innerText", span.innerText);
+          // console.log("span.classList", span.classList);
 
           usersToHide.forEach((userToHide) => {
             if (span.innerText == userToHide) {
-              console.log(
-                "span.innerText == userToHide:",
-                span.innerText == userToHide,
-              );
-              if (!span.classList.contains("hideThatUserMessage")) {
-                span.classList.add("hideThatUserMessage");
+              // console.log(
+              //   "span.innerText == userToHide:",
+              //   span.innerText == userToHide,
+              // );
+              if (!span.classList.contains("userToHide")) {
+                span.classList.add("userToHide");
               }
             }
           });
@@ -86,3 +83,16 @@ if (target) {
     characterData: true,
   });
 }
+
+console.log("Instructions : to hide a user, in console write");
+console.log('usersToHide.push("John DOE")');
+console.log("Instructions : to show a user, in console write");
+console.log("usersToHide = []");
+
+const style = document.createElement("style");
+style.id = "hideThoseUsers";
+style.textContent = `div[class*="fui-ChatMessage"]:has(span.userToHide),
+empty {
+    display: none;
+}`;
+document.head.appendChild(style);
